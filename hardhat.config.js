89593e472404d7f0a47e9840bc247b6924e12db6
0xAdnanH <0xAdnanH@gmail.com>
2023-08-18 11:53:24 +0300

    new file:   deploy/script.js 	new file:   deployments/Mumbai/.chainId 	new file:   deployments/Mumbai/SwapContract.json 	new file:   deployments/Mumbai/solcInputs/545f1ff0d4b957b1b515bcd057c16070.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
+require("hardhat-deploy");
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  networks: {
+    Mumbai: {
+      live: true,
+      chainId: 80001,
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [
+        "8e61e7662cc2bb10af0f30a4c8f284d0b4bdde2e1502574802eddbc6ace47aff",
+      ],
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai: "CCSKPAJJ874Y2Z537J4SFISNAP5C65PYP8",
+    },
+  },
+
+  polygonscan: {
+    apiKey: "CCSKPAJJ874Y2Z537J4SFISNAP5C65PYP8",
+    customChains: [
+      {
+        network: "polygonscan",
+        chainId: 80001,
+        urls: {
+          apiURL: "https://api-testnet.polygonscan.com/",
+          browserURL: "https://mumbai.polygonscan.com/",
+        },
+      },
+    ],
+  },
+  solidity: "0.8.9",
 };
